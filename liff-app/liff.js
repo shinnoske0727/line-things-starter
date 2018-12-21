ab3d963b54a440c18ac1ecd68bb03eb1443e3f1f
Shinnosuke Irie <kiyoshidainagon@gmail.com>
2018-12-21 14:14:51 +0900

    Update liff.js

--- liff-app/liff.js
+++ liff-app/liff.js
@@ -1,5 +1,5 @@
 // User service UUID: Change this to your generated service UUID
-const USER_SERVICE_UUID         = 'f9ba035a-d86d-4171-8936-ddffdcd0c1e2'; // LED, Button
+const USER_SERVICE_UUID         = 'F9BA035A-D86D-4171-8936-DDFFDCD0C1E2'; // LED, Button
 // User service characteristics
 const LED_CHARACTERISTIC_UUID   = 'E9062E71-9E62-4BC6-B0D3-35CDCD9B027B';
 const BTN_CHARACTERISTIC_UUID   = '62FBD229-6EDD-4D1A-B554-5C4E1BB29169';
